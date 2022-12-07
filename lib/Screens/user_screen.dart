9f7142e9c0604e092c204bda78d87d177e96cabe
Roger <79161186+Draquian@users.noreply.github.com>
2022-12-07 19:08:55 +0100

    User Info from fireBase

--- lib/Screens/user_screen.dart
+++ lib/Screens/user_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,30 +12,93 @@ class UserScreen extends StatefulWidget {
 class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text(
-          //TUDU: User = variable user del fireBase
-          "User",
-          style: TextStyle(
-            fontSize: 37,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 12,
-          ),
+        title: StreamBuilder(
+          stream: db.collection("/Users").snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.hasError) {
+              return ErrorWidget(snapshot.error.toString());
+            }
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final querySnap = snapshot.data!;
+            final docs = querySnap.docs;
+            return Text(
+              //TUDU: search current User, not first
+              docs.first['User'],
+              style: const TextStyle(
+                fontSize: 37,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 12,
+              ),
+            );
+          },
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-            )
-          ],
-        ),
+      body: StreamBuilder(
+        stream: db.collection("/Users").snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.hasError) {
+            return ErrorWidget(snapshot.error.toString());
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final querySnap = snapshot.data!;
+          final docs = querySnap.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final doc = docs[index];
+              final Timestamp t = doc['Last Time Play'];
+              return Column(
+                children: [
+                  Text(
+                    doc['Sudokus Complete'].toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    doc['Wins Online'].toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    doc['Win Streak'].toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    doc['Better Time'].toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    t.toDate().toString(),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.logout),
+                        onPressed: () {
+                          FirebaseAuth.instance.signOut();
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
