480e36a97a4d9dd9a7fd6ee49e512116d91cc96b
Roger <79161186+Draquian@users.noreply.github.com>
2022-12-09 23:31:25 +0100

    .

--- lib/Screens/waiting_screen.dart
+++ lib/Screens/waiting_screen.dart
@@ -7,35 +7,33 @@ class WaitingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DateTime _timer = DateTime.now();
     return Scaffold(
-      appBar: AppBar(
-          leading: BackArrowQuestion(
-              question: "Do you want to exit the queue?",
-              onExit: () {
-                //TUDU: erase from waiting players
-                Navigator.of(context).popUntil((route) => route.isFirst);
-              }),
-          backgroundColor: Colors.black,
-          title: Row(
-            children: [
-              const Text(
-                "WAITING",
-                style: TextStyle(
-                  fontSize: 37,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 12,
+        appBar: AppBar(
+            leading: BackArrowQuestion(
+                question: "Do you want to exit the queue?",
+                onExit: () {
+                  //TUDU: erase from waiting players
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                }),
+            backgroundColor: Colors.black,
+            title: Row(
+              children: [
+                const Text(
+                  "WAITING",
+                  style: TextStyle(
+                    fontSize: 37,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 12,
+                  ),
                 ),
-              ),
-              AnimatedTextKit(animatedTexts: [
-                TyperAnimatedText("...",
-                    textStyle: const TextStyle(
-                        fontSize: 37, fontWeight: FontWeight.bold),
-                    speed: const Duration(milliseconds: 500)),
-              ], repeatForever: true, pause: const Duration(milliseconds: 50))
-            ],
-          )),
-      body: null,
-    );
+                AnimatedTextKit(animatedTexts: [
+                  TyperAnimatedText("...",
+                      textStyle: const TextStyle(
+                          fontSize: 37, fontWeight: FontWeight.bold),
+                      speed: const Duration(milliseconds: 500)),
+                ], repeatForever: true, pause: const Duration(milliseconds: 50))
+              ],
+            )),
+        body: null);
   }
 }
