694a100f1111faaf66651a36ae1f55d1b97f7cf8
Roger <79161186+Draquian@users.noreply.github.com>
2022-12-09 23:22:54 +0100

    waiting text animation

--- lib/Screens/waiting_screen.dart
+++ lib/Screens/waiting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:nodefirstproj/Widget/back_arrow_question.dart';
 
@@ -6,24 +7,34 @@ class WaitingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime _timer = DateTime.now();
     return Scaffold(
       appBar: AppBar(
-        leading: BackArrowQuestion(
-            question: "Do you want to exit the queue?",
-            onExit: () {
-              //TUDU: erase from waiting players
-              Navigator.of(context).popUntil((route) => route.isFirst);
-            }),
-        backgroundColor: Colors.black,
-        title: const Text(
-          "WAITING...",
-          style: TextStyle(
-            fontSize: 37,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 12,
-          ),
-        ),
-      ),
+          leading: BackArrowQuestion(
+              question: "Do you want to exit the queue?",
+              onExit: () {
+                //TUDU: erase from waiting players
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              }),
+          backgroundColor: Colors.black,
+          title: Row(
+            children: [
+              const Text(
+                "WAITING",
+                style: TextStyle(
+                  fontSize: 37,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 12,
+                ),
+              ),
+              AnimatedTextKit(animatedTexts: [
+                TyperAnimatedText("...",
+                    textStyle: const TextStyle(
+                        fontSize: 37, fontWeight: FontWeight.bold),
+                    speed: const Duration(milliseconds: 500)),
+              ], repeatForever: true, pause: const Duration(milliseconds: 50))
+            ],
+          )),
       body: null,
     );
   }
