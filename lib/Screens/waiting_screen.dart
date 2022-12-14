8612b9ac613f5cfdc6b1ed03c9ff467b980b9334
Roger <rogersalas2000@gmail.com>
2022-12-14 12:24:42 +0100

    waiting online

--- lib/Screens/waiting_screen.dart
+++ lib/Screens/waiting_screen.dart
@@ -1,5 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nodefirstproj/Screens/menu_screen.dart';
 import 'package:nodefirstproj/Widget/back_arrow_question.dart';
 //import 'dart:math';
 
@@ -8,6 +10,7 @@ class WaitingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
     return Scaffold(
       appBar: AppBar(
           leading: BackArrowQuestion(
@@ -35,76 +38,75 @@ class WaitingScreen extends StatelessWidget {
               ], repeatForever: true, pause: const Duration(milliseconds: 50))
             ],
           )),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 150.0),
-            child: Row(
-              children: const [
-                Text(
-                  "Room X",
-                  style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic),
+      body: StreamBuilder(
+          stream: db
+              .collection("/TotalRoomsOnline/GtHieM2C5bA4WCxTUc4y/UsersInRoom")
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.hasError) {
+              return ErrorWidget(snapshot.error.toString());
+            }
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final querySnap = snapshot.data!;
+            final docs = querySnap.docs;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 150.0),
+                  child: Row(
+                    children: const [
+                      Text(
+                        "Room X",
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                for (var i = 0; i < docs.length; i++)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("-\t"),
+                      Text(
+                        docs[i]["userName"],
+                        style: const TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("-\t"),
+                    Text(
+                      "...",
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+                MenuButton(
+                  onClick: (() {}),
+                  text: "Start",
+                  icon: Icons.logout,
                 ),
               ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("-\t"),
-              Text(
-                "Player 1",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("-\t"),
-              Text(
-                "Player 2",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("-\t"),
-              Text(
-                "...",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("-\t"),
-              Text(
-                "...",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
