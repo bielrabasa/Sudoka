7f9c2deff7821011f2c0c9d7f9b7345cc912d8f0
Roger <79161186+Draquian@users.noreply.github.com>
2023-01-27 19:57:41 +0100

    ranking

--- lib/Screens/ranking_screen.dart
+++ lib/Screens/ranking_screen.dart
@@ -23,56 +23,126 @@ class RankingScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: PlayerSnapshots(
-        roomId: "GtHieM2C5bA4WCxTUc4y",
-        builder: (List<Player> players) => Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 50),
-                Column(
-                  children: [
-                    for (var i = 0; i < players.length; i++)
-                      DocSnapBuilder(
-                        docRef: db.doc("/Users/${players[i].userId}"),
-                        builder: (
-                          BuildContext context,
-                          DocumentSnapshot<Map<String, dynamic>> doc,
-                        ) {
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                doc['Name'],
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                ),
-                              ),
-                              const SizedBox(width: 100),
-                              Text(
-                                players[i].totalTime.toString(),
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                ),
-                              ),
-                              // ignore: prefer_const_constructors
-                              Text(
-                                " Seconds",
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                ),
-                              ),
-                            ],
-                          );
-                        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 100.0),
+        child: PlayerSnapshots(
+          roomId: "GtHieM2C5bA4WCxTUc4y",
+          builder: (List<Player> players) => Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          for (var i = 0; i < players.length; i++)
+                            DocSnapBuilder(
+                              docRef: db.doc("/Users/${players[i].userId}"),
+                              builder: (
+                                BuildContext context,
+                                DocumentSnapshot<Map<String, dynamic>> doc,
+                              ) {
+                                return Column(
+                                  children: [
+                                    Text(
+                                      doc['Name'],
+                                      style: const TextStyle(
+                                        fontSize: 25,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 50),
+                                  ],
+                                );
+                              },
+                            ),
+                        ],
                       ),
-                  ],
-                ),
-                const SizedBox(height: 50),
-              ],
-            ),
-          ],
+                      //const SizedBox(width: 100),
+                      Column(
+                        children: [
+                          for (var i = 0; i < players.length; i++)
+                            DocSnapBuilder(
+                              docRef: db.doc("/Users/${players[i].userId}"),
+                              builder: (
+                                BuildContext context,
+                                DocumentSnapshot<Map<String, dynamic>> doc,
+                              ) {
+                                return Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          players[i].totalTime.toString(),
+                                          style: const TextStyle(
+                                            fontSize: 25,
+                                          ),
+                                        ),
+                                        // ignore: prefer_const_constructors
+                                        Text(
+                                          "''",
+                                          style: const TextStyle(
+                                            fontSize: 25,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 50),
+                                  ],
+                                );
+                              },
+                            ),
+                        ],
+                      )
+                    ],
+                  ),
+                  /*Column(
+                    children: [
+                      for (var i = 0; i < players.length; i++)
+                        DocSnapBuilder(
+                          docRef: db.doc("/Users/${players[i].userId}"),
+                          builder: (
+                            BuildContext context,
+                            DocumentSnapshot<Map<String, dynamic>> doc,
+                          ) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  doc['Name'],
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                                const SizedBox(width: 100),
+                                Text(
+                                  players[i].totalTime.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                                // ignore: prefer_const_constructors
+                                Text(
+                                  " Seconds",
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                    ],
+                  ),*/
+                  const SizedBox(height: 50),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
