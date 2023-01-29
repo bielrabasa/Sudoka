e11496f14d7f267cebfa3481355d39e010daf3fc
Biel Rabasa <bielrabasa@gmail.com>
2023-01-29 15:28:34 +0100

    Ranking screen REWORK

--- lib/Screens/ranking_screen.dart
+++ lib/Screens/ranking_screen.dart
@@ -8,6 +8,14 @@ import '../model/player.dart';
 class RankingScreen extends StatelessWidget {
   const RankingScreen({super.key});
 
+  int playersFinished(List<Player> players){
+    int pf = 0;
+    for(int i = 0; i<players.length; i++){
+      if(players[i].hasFinished) pf++;
+    }
+    return pf;
+  }
+
   @override
   Widget build(BuildContext context) {
     final db = FirebaseFirestore.instance;
@@ -23,110 +31,111 @@ class RankingScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 100.0),
-        child: PlayerSnapshots(
-          roomId: "GtHieM2C5bA4WCxTUc4y",
-          builder: (List<Player> players) => Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  const SizedBox(height: 50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          for (var i = 0; i < players.length; i++)
-                            DocSnapBuilder(
-                              docRef: db.doc("/Users/${players[i].userId}"),
-                              builder: (
-                                BuildContext context,
-                                DocumentSnapshot<Map<String, dynamic>> doc,
-                              ) {
-                                return Column(
-                                  children: [
-                                    if (players[i].totalTime != 0)
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          if (i == 0)
-                                            const Icon(Icons.filter_1,
-                                                color: Color.fromARGB(
-                                                    255, 255, 223, 0)),
-                                          if (i == 1)
-                                            const Icon(Icons.filter_2,
-                                                color: Color.fromARGB(
-                                                    255, 168, 169, 173)),
-                                          if (i == 2)
-                                            const Icon(Icons.filter_3,
-                                                color: Color.fromARGB(
-                                                    255, 205, 127, 50)),
-                                          const SizedBox(height: 10, width: 15),
-                                          Text(
-                                            doc['Name'],
-                                            style: const TextStyle(
-                                              fontSize: 25,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    const SizedBox(height: 50),
-                                  ],
-                                );
-                              },
-                            ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          for (var i = 0; i < players.length; i++)
-                            DocSnapBuilder(
-                              docRef: db.doc("/Users/${players[i].userId}"),
-                              builder: (
-                                BuildContext context,
-                                DocumentSnapshot<Map<String, dynamic>> doc,
-                              ) {
-                                return Column(
-                                  children: [
-                                    if (players[i].totalTime != 0)
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            players[i].totalTime.toString(),
-                                            style: const TextStyle(
-                                              fontSize: 25,
-                                            ),
-                                          ),
-                                          // ignore: prefer_const_constructors
-                                          Text(
-                                            "''",
-                                            style: const TextStyle(
-                                              fontSize: 25,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    const SizedBox(height: 50),
-                                  ],
-                                );
-                              },
-                            ),
-                        ],
-                      )
-                    ],
+      body: PlayerSnapshots(
+        roomId: "GtHieM2C5bA4WCxTUc4y",
+        builder: (List<Player> players) {
+          int iterator = 0;
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < players.length; i++)
+                  DocSnapBuilder(
+                    docRef: db.doc("/Users/${players[i].userId}"),
+                    builder: (
+                      BuildContext context,
+                      DocumentSnapshot<Map<String, dynamic>> doc,
+                    ) {
+                      if (players[i].totalTime == 0) {
+                        return const SizedBox(width: 0, height: 0);
+                      }
+
+                      if(playersFinished(players) <= iterator){
+                        iterator = 0;
+                      }
+
+                      iterator++;
+                      return playerRankRow(
+                        rank: iterator,
+                        name: doc['Name'],
+                        time: players[i].totalTime,
+                      );
+                    },
                   ),
-                  const SizedBox(height: 50),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class playerRankRow extends StatelessWidget {
+  final int rank;
+  final String name;
+  final num time;
+
+  const playerRankRow({
+    Key? key,
+    required this.rank,
+    required this.name,
+    required this.time,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 300,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //icon
+              if (rank == 1)
+                const Icon(Icons.filter_1,
+                    color: Color.fromARGB(255, 255, 223, 0)),
+              if (rank == 2)
+                const Icon(Icons.filter_2,
+                    color: Color.fromARGB(255, 168, 169, 173)),
+              if (rank == 3)
+                const Icon(Icons.filter_3,
+                    color: Color.fromARGB(255, 205, 127, 50)),
+              if (rank > 3) const SizedBox(width: 20),
+
+              //name
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+
+              //time
+              Row(
+                children: [
+                  Text(
+                    time.toString(),
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                  const Text(
+                    "''",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
                 ],
               ),
             ],
           ),
         ),
-      ),
+        const SizedBox(height: 30),
+      ],
     );
   }
 }
